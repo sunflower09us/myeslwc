2c19ba52c13c1d4434a082b935908bd1a6f7fa48
Hui Li <hui.li@veeva.com>
2020-08-31 19:17:16 -0700

    space management

---
+++ es-space-mgmt/main/default/lwc/customerList/customerList.js
@@ -0,0 +1,23 @@
+import { LightningElement, api, wire } from 'lwc';
+import getCustomerList from '@salesforce/apex/reservationManagerController.getCustomerList';
+export default class CustomerList extends LightningElement {
+    customers = [];
+
+    @api sobject = 'Lead';
+    errorMsg;
+    msgForUser;
+    wiredRecords;
+
+
+    @wire(getCustomerList, { sObjectType: '$sobject' })
+    wiredCustomerData(value) {
+        console.log(JSON.stringify(value));
+        this.wiredRecords = value;
+        if (value.error) {
+            this.errorMsg = value.error;
+            this.msgForUser = 'There was an issue loading customers.';
+        } else if (value.data) {
+            this.customers = value.data;
+        }
+    }
+}
